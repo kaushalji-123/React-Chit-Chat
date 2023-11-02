b9d3da88dff3647bfa087c6354748f151d34b91c
kaushalji-123 <divyanshu838283fb@gmail.com>
2023-11-02 13:22:16 +0530

    Added Sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
 import { useState } from 'react'
-import './App.css'
+import Register from './Components/Register'
+import "./App.scss"
 
 function App() {
 
   return (
     <>
-      <div className="formContainer">
-        <div className="formWrapper">
-          <span className='logo'>Papen Chat</span>
-          <span className='title'>Register</span>
-          <form>
-            <input type="text" placeholder='UserName'/>
-            <input type="text" placeholder='Email' />
-            <input type="text" placeholder='Password'/>
-            <input type="file"
-            aria-label='' placeholder='Add Your Avatar'/>
-            <button>Sign Up </button>
-            <p>You Do Have an Account ? login</p>
-          </form>
-        </div>
-      </div>
+    <h1>Hello react with vite and scss</h1>
+    <Register/>
     </>
   )
 }
